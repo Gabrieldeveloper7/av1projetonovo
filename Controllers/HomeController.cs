b2be4c3cdbe331c7c44fc6e8c1aa34c27dbd1511
Gabriel <biel9875@uni9.edu.br>
2022-11-29 16:56:18 -0300

    meu primeiro commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using av1projeto.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace av1projeto.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            HomeModel home = new HomeModel();
+
+            home.Nome = "Gabriel de Souza da Silva";
+            home.Email = "biel9875@uni9.edu.br";
+
+            return View(home);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
